24fe7ff73591f80f6a4d2cd6c4c2cd76847c7120
benjig-winbenihana <benbjammin@gmail.com>
2017-02-05 20:23:53 -0700

    Clean up organize files / folders

--- js/exercise-one-temp-tests.js
+++ js/exercise-one-temp-tests.js
@@ -6,11 +6,6 @@ exports.exerciseOneMethods = {
     return string;
   },
 
-  // Write some code that will accept an amount and convert it to the appropriate string representation.
-  // Example:
-  // Convert 2523.04
-  // to "Two thousand five hundred twenty-three and 04/100 dollars"
-
   inputAmtToString: function(amt) {
     var amount = toString(amt);
     return amount;
@@ -32,7 +27,4 @@ exports.exerciseOneMethods = {
 
     return amtAsNum;
   }
-
-
-
 };
