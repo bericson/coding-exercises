6d5b7881641bd1c330a363c7cc9c3f7dfa1e0867
benjig-win\benihana <benbjammin@gmail.com>
2017-02-05 20:16:02 -0700

    Build-out functionality to process any decimal portion on the amount entered and append it to the converted integer words for display; refactor / clean up JS code a bit; refine input field behavior in UI

--- app/js/index.js
+++ app/js/index.js
@@ -1,11 +1,12 @@
-
-// Write some code that will accept an amount and convert it to the appropriate string representation.
-// Example:
-// Convert 2523.04
-// to "Two thousand five hundred twenty-three and 04/100 dollars"
+////////
+//// Write some code that will accept an amount and convert it to the appropriate string representation.
+//// Example:
+//// Convert 2523.04
+//// to "Two thousand five hundred twenty-three and 04/100 dollars"
+////////
 
 //// Todos:
-// Abstract-out the 'NumberWordMap' object to be an imported module
+// Abstract-out the 'NumberWordMap' object to be an imported module?
 
 (function() {
 
@@ -59,7 +60,7 @@
 			.set(1, 'trillion')
 	}
 
-	function parseInput(input) {
+	function parseProcessInput(input) {
 		// console.log("raw input:", input);
 		// console.log("raw input typeof:", typeof input);
 		
@@ -76,9 +77,8 @@
 		// console.log("rawDecimal:", rawDecimal);
 		// console.log("rawDecimal typeof:", typeof rawDecimal);
 		
-
 		//////// Now that the integer and decimal components have been isolated, process 
-		//// them in a separate but parallel fashion
+		//// them separately
 
 		////// Process integer component
 		//// Split integer into array of strings
@@ -95,62 +95,31 @@
 		////// Process decimal component		
 		//// Round the decimal to two places, remove the leading zero and the decimal point
 		var filteredDecimal = rawDecimal.toFixed(2).slice(2, 4);
-
-		//// Split decimal into array of strings
-		var filteredDecimalStringArr = filteredDecimal.split('');
-		// console.log("filteredDecimal:", filteredDecimal);
-		// console.log("filteredDecimal typeof:", typeof filteredDecimal);
-		// console.log("filteredDecimalStringArr:", filteredDecimalStringArr);
-		// console.log("filteredDecimalStringArr typeof:", typeof filteredDecimalStringArr);
-
-		//// Convert the array of decimals as strings into array of decimals as numbers
-		var decimalNumArr = [];
-		for (var i in filteredDecimalStringArr) {
-			decimalNumArr[i] = parseInt(filteredDecimalStringArr[i], 10);
-		}
-		//console.log("decimalNumArr:", decimalNumArr);
 		
 		var convertedIntegerToWords = convertIntegerToWords(integerNumArr);
-		console.log("convertedIntegerToWords:", convertedIntegerToWords);
-		return convertedIntegerToWords;
-
-		//return integerNumArr;
-		// convertDecimalToFraction(decimalNumArr);
-
-		//return integerNumArr.concat(decimalNumArr);
+		//console.log("convertedIntegerToWords:", convertedIntegerToWords);
+		
+		var decimalValue = filteredDecimal != 00 ?  ' and ' + filteredDecimal + '/100' : '';
+		return convertedIntegerToWords + decimalValue + ' dollars';
 	}
-
-	//function convertDecimalToFraction(decimalInput) {}
 
 	//var initialInput = "9";
 	//var initialInput = "29";
-	//var initialInput = "100";
-	var initialInput = "9220090";
+	//var initialInput = "129";
+	//var initialInput = "7129";
 	//var initialInput = "17129";
 	//var initialInput = "617129";
 	//var initialInput = "3617129";
 	//var initialInput = "53617129";
-	//var initialInput = "853617100";
-	//var initialInput = "123.54333";
-	//var initialInput = "123456789101112.54333";
-	mainProgram(initialInput);
+	//var initialInput = "853617129";
+	//var initialInput = "123234.04333";
+	//mainProgram(initialInput);
 
 	function convertIntegerToWords(integerInputArr) {
-		// integerInput == 123456789101112
-		// integerInput == 123,456,789,101,112
-		// integerNumArr: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 0, 1, 1, 1, 2 ] 
-		// integerNumArr.length == 15
-		// 15, 14, 13 // 12, 11, 10 // 9, 8 , 7 // 6, 5, 4 // 3, 2, 1
-		// trillion, billion, million, thousand, hundred, ten, one
-		//placeHolderArr = [ "trillion", "billion", "million", "thousand", "hundred", "ten", "one" ];
+		var arrLength = integerInputArr.length, convertedResult;
 
-		var i = 0,
-				arrLength = integerInputArr.length,
-				convertedResult,
-				sliceRangeHundred;
-
-		console.log("initialInput:", integerInputArr);
-		console.log("convertIntegerToWords arrLength:", arrLength);
+		// console.log("initialInput:", integerInputArr);
+		// console.log("convertIntegerToWords arrLength:", arrLength);
 
 		switch(arrLength) {
 			case 1:
@@ -231,228 +200,195 @@
 													getHundredRangeName(integerInputArr.slice(-3, 7)) + ' ' + 
 													convertTensTeensOrOnesRange(integerInputArr.slice(-2, 9));
 			 	break;
-			// case 10:
-			// case 11:
-			// case 12:
-			// case 13:
-			// case 14:
-			// case 15:
-			// 	break;
 			default:
 				var outOfRangeMsg = "Amount not in range ...";
-				console.log(outOfRangeMsg);
+				//console.log(outOfRangeMsg);
 				convertedResult = outOfRangeMsg;
 				break;			
 		}
 
-		function convertOnesRange(inputArr) {
-			console.log("convertOnesRange inputArr:", inputArr);
-			
-			var i = 0, 
-					localizedInputArr = inputArr, 
-					localArrLen = localizedInputArr.length;
-
-			console.log("convertOnesRange localizedInputArr:", localizedInputArr);
-			console.log("convertOnesRange localArrLen:", localArrLen);
-
-			var onesRangeNumName;
-
-			if (localizedInputArr[0] === 0) {
-				onesRangeNumName = '';
-			}
-			else {				
-				for (i; i < localArrLen; i++) {
-					onesRangeNumName = 	NumberWordMap.baseOne.get(localizedInputArr[i]);
-					console.log("onesRangeNumName:", ucFirst(onesRangeNumName));
-				}
-			}
-
-			return onesRangeNumName;
-		}
-
-		function convertTensRange(inputArr) {
-			console.log("convertTensRange inputArr:", inputArr);
-			
-			var i = 0, 
-					localizedInputArr = inputArr, 
-					localArrLen = localizedInputArr.length;
-
-			console.log("convertTensRange localizedInputArr:", localizedInputArr);
-			console.log("convertTensRange localArrLen:", localArrLen);
-
-			for (i; i < localArrLen; i++) {
-				var tensRangeNumName = 	NumberWordMap.baseTen.get(localizedInputArr[i]);
-				console.log("tensRangeNumName:", ucFirst(tensRangeNumName));
-			}
-
-			return tensRangeNumName;
-		}
-
-		function convertTeensRange(inputArr) {
-			console.log("convertTeensRange inputArr:", inputArr);
-			
-			var i = 0, 
-					localizedInputArr = inputArr, 
-					localArrLen = localizedInputArr.length;
-
-			console.log("convertTeensRange localizedInputArr:", localizedInputArr);
-			console.log("convertTeensRange localArrLen:", localArrLen);
-
-			for (i; i < localArrLen; i++) {
-				var teensRangeNumName = 	NumberWordMap.baseTeen.get(localizedInputArr[i]);
-				console.log("teensRangeNumName:", ucFirst(teensRangeNumName));
-			}
-
-			return teensRangeNumName;
-		}
-
-		function convertTensOrTeensRange(inputArr) {
-			console.log("convertTensOrTeensRange inputArr:", inputArr);
-			
-			var i = 0, 
-					localizedInputArr = inputArr, 
-					localArrLen = localizedInputArr.length,
-					result;
-
-			if (localizedInputArr[0] === 0 && localizedInputArr[1] === 0) { result = ''; }
-			else if (localizedInputArr[0] === 1 && localizedInputArr[1] === 0) {
-				result = convertTensRange(localizedInputArr.slice(-2, 1));
-			}  
-			else if (localizedInputArr[0] === 1 && localizedInputArr[1] !== 0) {
-				result = convertTeensRange(localizedInputArr.slice(-1, 2));
-			}
-			else {
-				result = convertTensRange(localizedInputArr.slice(-2, 1)) + ' ' +
-									convertOnesRange(localizedInputArr.slice(-1, 2));
-			}
-
-			return result;
-		}
-
-		function convertTensTeensOrOnesRange(inputArr) {
-			console.log("convertTensTeensOrOnesRange inputArr:", inputArr);
-			
-			var i = 0, 
-					localizedInputArr = inputArr, 
-					localArrLen = localizedInputArr.length,
-					result;			
-
-			if (localizedInputArr[0] === 0 && localizedInputArr[1] === 0) { result = ''; } 
-			else if (localizedInputArr[0] >= 1 && localizedInputArr[1] === 0) {
-				result = convertTensRange(localizedInputArr.slice(-2, 1));;
-			} 
-			else if (localizedInputArr[0] === 1 && localizedInputArr[1] !== 0) {
-				result = convertTeensRange(localizedInputArr.slice(-1, 2));
-			} 
-			else if (localizedInputArr[0] >= 1) {
-				result = convertTensRange(localizedInputArr.slice(-2, 1)) + ' ' +
-									convertOnesRange(localizedInputArr.slice(-1, 2));
-			}
-			else {
-				result = convertOnesRange(localizedInputArr.slice(-1, 2));
-			}
-
-			return result;
-		}
-
-		function sumArr(array) {
-			var summedArray = array.reduce(function(a, b) { return a + b; }, 0);
-			console.log("summedArray:", summedArray);
-			return summedArray;
-		}
-
-		function getHundredRangeName(inputArr) {
-			var result;
-			if (sumArr(inputArr) === 0) { result = ''; }
-			else {			
-				var hundredRangeName = NumberWordMap.baseHundred.get(1);
-				console.log("hundredRangeName:", ucFirst(hundredRangeName));
-				result = hundredRangeName;	
-			}
-
-			return result;
-		}
-
-		function getThousandRangeName(inputArr) {
-			var result;
-			if (sumArr(inputArr) === 0) { result = ''; }
-			else {			
-				var thousandRangeName = NumberWordMap.baseThousand.get(1);
-				console.log("thousandRangeName:", ucFirst(thousandRangeName));
-				result = thousandRangeName;	
-			}
-
-			return result;
-		}
-
-		function getMillionRangeName(inputArr) {
-			var result;
-			if (sumArr(inputArr) === 0) { result = ''; }
-			else {			
-				var millionRangeName = NumberWordMap.baseMillion.get(1);
-				console.log("millionRangeName:", ucFirst(millionRangeName));
-				result = millionRangeName;	
-			}
-
-			return result;
-		}
-
-		function getBillionRangeName(inputArr) {
-			var result;
-			if (sumArr(inputArr) === 0) { result = ''; }
-			else {			
-				var billionRangeName = NumberWordMap.baseBillion.get(1);
-				console.log("billionsRangeName:", ucFirst(billionRangeName));
-				result = billionRangeName;	
-			}
-
-			return result;
-		}
-
-		function getTrllionRangeName(inputArr) {
-			var result;
-			if (sumArr(inputArr) === 0) { result = ''; }
-			else {
-				var trillionRangeName = NumberWordMap.baseTrillion.get(1);
-				console.log("trillionsRangeName:", ucFirst(trillionRangeName));
-				result = trillionRangeName;				
-			}
-
-			return result;
-		}
-
 		return ucFirst(convertedResult);
 	};
+
+	function convertOnesRange(inputArr) {		
+		var i = 0, arrLen = inputArr.length, onesRangeNumName;
+		//console.log("convertOnesRange inputArr:", inputArr);
+		//console.log("convertOnesRange arrLen:", arrLen);
+		if (inputArr[0] === 0) {
+			onesRangeNumName = '';
+		}
+		else {				
+			for (i; i < arrLen; i++) {
+				onesRangeNumName = 	NumberWordMap.baseOne.get(inputArr[i]);
+				//console.log("onesRangeNumName:", ucFirst(onesRangeNumName));
+			}
+		}
+
+		return onesRangeNumName;
+	}
+
+	function convertTensRange(inputArr) {		
+		var i = 0, arrLen = inputArr.length;		
+		//console.log("convertTensRange inputArr:", inputArr);
+		//console.log("convertTensRange arrLen:", arrLen);		
+		for (i; i < arrLen; i++) {
+			var tensRangeNumName = 	NumberWordMap.baseTen.get(inputArr[i]);
+			//console.log("tensRangeNumName:", ucFirst(tensRangeNumName));
+		}
+
+		return tensRangeNumName;
+	}
+
+	function convertTeensRange(inputArr) {		
+		var i = 0, arrLen = inputArr.length;		
+		//console.log("convertTeensRange inputArr:", inputArr);
+		//console.log("convertTeensRange arrLen:", arrLen);
+		for (i; i < arrLen; i++) {
+			var teensRangeNumName = 	NumberWordMap.baseTeen.get(inputArr[i]);
+			//console.log("teensRangeNumName:", ucFirst(teensRangeNumName));
+		}
+
+		return teensRangeNumName;
+	}
+
+	function convertTensOrTeensRange(inputArr) {		
+		var result;
+		//console.log("convertTensOrTeensRange inputArr:", inputArr);		
+		if (inputArr[0] === 0 && inputArr[1] === 0) { result = ''; }
+		else if (inputArr[0] === 1 && inputArr[1] === 0) {
+			result = convertTensRange(inputArr.slice(-2, 1));
+		}  
+		else if (inputArr[0] === 1 && inputArr[1] !== 0) {
+			result = convertTeensRange(inputArr.slice(-1, 2));
+		}
+		else {
+			result = 	convertTensRange(inputArr.slice(-2, 1)) + ' ' +
+								convertOnesRange(inputArr.slice(-1, 2));
+		}
+
+		return result;
+	}
+
+	function convertTensTeensOrOnesRange(inputArr) {		
+		var result;
+		//console.log("convertTensTeensOrOnesRange inputArr:", inputArr);
+		if (inputArr[0] === 0 && inputArr[1] === 0) { result = ''; } 
+		else if (inputArr[0] >= 1 && inputArr[1] === 0) {
+			result = convertTensRange(inputArr.slice(-2, 1));;
+		} 
+		else if (inputArr[0] === 1 && inputArr[1] !== 0) {
+			result = convertTeensRange(inputArr.slice(-1, 2));
+		} 
+		else if (inputArr[0] >= 1) {
+			result = convertTensRange(inputArr.slice(-2, 1)) + ' ' +
+								convertOnesRange(inputArr.slice(-1, 2));
+		}
+		else {
+			result = convertOnesRange(inputArr.slice(-1, 2));
+		}
+
+		return result;
+	}
+
+	function sumArr(array) {
+		var summedArray = array.reduce(function(a, b) { return a + b; }, 0);
+		//console.log("summedArray:", summedArray);
+		return summedArray;
+	}
+
+	function getHundredRangeName(inputArr) {
+		var result;
+		//console.log("getHundredRangeName inputArr:", inputArr);
+		if (sumArr(inputArr) === 0) { result = ''; }
+		else {			
+			var hundredRangeName = NumberWordMap.baseHundred.get(1);
+			//console.log("hundredRangeName:", ucFirst(hundredRangeName));
+			result = hundredRangeName;	
+		}
+
+		return result;
+	}
+
+	function getThousandRangeName(inputArr) {
+		var result;
+		//console.log("getThousandRangeName inputArr:", inputArr);
+		if (sumArr(inputArr) === 0) { result = ''; }
+		else {			
+			var thousandRangeName = NumberWordMap.baseThousand.get(1);
+			//console.log("thousandRangeName:", ucFirst(thousandRangeName));
+			result = thousandRangeName;	
+		}
+
+		return result;
+	}
+
+	function getMillionRangeName(inputArr) {
+		var result;
+		//console.log("getMillionRangeName inputArr:", inputArr);
+		if (sumArr(inputArr) === 0) { result = ''; }
+		else {			
+			var millionRangeName = NumberWordMap.baseMillion.get(1);
+			//console.log("millionRangeName:", ucFirst(millionRangeName));
+			result = millionRangeName;	
+		}
+
+		return result;
+	}
+
+	function getBillionRangeName(inputArr) {
+		var result;
+		//console.log("getBillionRangeName inputArr:", inputArr);
+		if (sumArr(inputArr) === 0) { result = ''; }
+		else {			
+			var billionRangeName = NumberWordMap.baseBillion.get(1);
+			//console.log("billionsRangeName:", ucFirst(billionRangeName));
+			result = billionRangeName;	
+		}
+
+		return result;
+	}
+
+	function getTrllionRangeName(inputArr) {
+		var result;
+		//console.log("getTrllionRangeName inputArr:", inputArr);
+		if (sumArr(inputArr) === 0) { result = ''; }
+		else {
+			var trillionRangeName = NumberWordMap.baseTrillion.get(1);
+			//console.log("trillionsRangeName:", ucFirst(trillionRangeName));
+			result = trillionRangeName;				
+		}
+
+		return result;
+	}
 
 	function ucFirst(string) {
 		return string.charAt(0).toUpperCase() + string.slice(1);
 	}
 
 	function mainProgram(inputAmt) {
-		//parseInput(inputAmt);
-		var parsedInput = parseInput(inputAmt);
-		console.log("parsedInput:", parsedInput);		
+		var parsedProcessedInput = parseProcessInput(inputAmt);
+		console.log("parsedAndProcessedInput:", parsedProcessedInput);
 
 		$("#ConvertedResult").css("display", "block");
 		$("#ConvertedResult div:first-child span").text(inputAmt);
-		$("#ConvertedResult div:last-child span").text(parsedInput);
+		$("#ConvertedResult div:last-child span").text(parsedProcessedInput);
 		$("#InputAmt").val('');
 	}
 
 	$(function() {
 		$("form").on("submit", function(e) {
 			e.preventDefault();
-			var userInputAmt = $("#InputAmt").val();
+			var userInputAmt = $("#InputAmt").val();			
+			//console.log(userInputAmt);
 			if (!userInputAmt) {
 				alert("Please enter an amount to convert in the input field.");
 				return;
 			}
-			//console.log(userInputAmt);
 			mainProgram(userInputAmt);
 		});
 
-		// Crossbrowser workaround for limiting numbers/characters allowed in input field
-		// Source: http://codepen.io/wuori/pen/LNyYBM
+		//// Crossbrowser workaround for actively limiting numbers/characters allowed 
+		//// in input field as they are entered - source: http://codepen.io/wuori/pen/LNyYBM
 		$('input[type="number"]').each(function(){
 			$(this).on('keyup',function(){
 				if ($(this).val() > Number($(this).attr("max"))) {
@@ -461,6 +397,7 @@
         }
 			});
 		});
+
 	});
 
 })();
